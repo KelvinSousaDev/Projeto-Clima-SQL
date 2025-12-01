912ac92034eea030eb300411028bf110b61e32bb
KelvinSousaDev <kelvinsousa.dev@gmail.com>
2025-12-01 20:19:14 -0300

    Feat: Dashboard visual finalizado com Gráficos e KPIs

--- dashboard_clima.py
+++ dashboard_clima.py
@@ -4,8 +4,9 @@ from dotenv import load_dotenv
 import pandas as pd
 import streamlit as st
 
-load_dotenv()
+st.set_page_config(page_title="Clima Tracker Global", layout="wide")
 
+load_dotenv()
 url_banco = os.getenv("DATABASE_URL")
 
 @st.cache_resource
@@ -13,15 +14,49 @@ def init_connection():
   print("📡 Conectando ao Neon PostgreSQL...")
   return psycopg2.connect(url_banco)
 
-st.set_page_config(page_title="Dados Climaticos", layout="centered")
-
-st.title("💸 Registro de Dados")
-st.write("Visão geral dos dados importados do Banco de Dados SQL.")
+st.title("🌍 Monitoramento Climático em Tempo Real")
+st.markdown("Dados coletados via API e armazenados no Neon PostgreSQL (Cloud).")
 
 try:
   conn = init_connection()
+
   query = "SELECT * FROM clima_historico ORDER BY data_hora DESC"
   df = pd.read_sql(query, conn)
-  st.dataframe(df)
+
+  if not df.empty:
+     ultimos = df.drop_duplicates(subset=['cidade'], keep='first')
+
+     cols = st.columns(len(ultimos))
+
+     for i, (index, linha) in enumerate(ultimos.iterrows()):
+            cidade = linha['cidade']
+            temp = f"{linha['temperatura']}°C"
+            hora = linha['data_hora'].strftime('%H:%M')
+
+            cols[i].metric(label=cidade, value=temp, delta=f"às {hora}")
+  else:
+        st.warning("Aguardando dados...")
+
+  st.divider()
+
+  aba_grafico, aba_dados = st.tabs(["📈 Histórico Visual", "📋 Dados Brutos"])
+
+  with aba_grafico:
+        if not df.empty:
+            st.subheader("Evolução da Temperatura")
+            
+            df_chart = df.pivot_table(index='data_hora', columns='cidade', values='temperatura')
+            
+            st.line_chart(df_chart)
+        else:
+            st.info("O gráfico aparecerá assim que houver dados.")
+
+  with aba_dados:
+      st.dataframe(df, use_container_width=True)
+
+  if st.button("🔄 Atualizar Dados"):
+      st.cache_resource.clear()
+      st.rerun()
+
 except Exception as e:
-    st.error(f"❌ Erro ao conectar no banco: {e}")
+    st.error(f"❌ Erro ao conectar: {e}")
